81a583368fc595023987e078d835c8260fc091e6
Josh Courtney <joshrcourtney@gmail.com>
2021-05-17 10:51:07 -0500

    Setup UI. Add preliminary code

--- see-food/ViewController.swift
+++ see-food/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePickerVC = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePickerVC.delegate = self
+        imagePickerVC.sourceType = .camera
+        imagePickerVC.allowsEditing = false
     }
 
-
+    @IBAction func cameraBtnPressed(_ sender: UIBarButtonItem) {
+        present(imagePickerVC, animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        do {
+            let model = try VNCoreMLModel(for: Inceptionv3().model)
+            let request = VNCoreMLRequest(model: model) { request, error in
+                guard let results = request.results as? [VNClassificationObservation] else {
+                    fatalError()
+                }
+                
+                print(results)
+            }
+            
+            let handler = VNImageRequestHandler(ciImage: image)
+            
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
 }
 
+extension ViewController: UINavigationControllerDelegate {
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError()
+            }
+            
+            detect(image: ciImage)
+        }
+        
+        imagePickerVC.dismiss(animated: true, completion: nil)
+    }
+}
